cd02da0e07c330b5f51d7e8af01059e8b7cedf04
fimac <fiona@cipherstash.com>
2022-06-16 21:00:27 +1000

    update env var config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative "application"
 
+#load test_set.rb
+app_test = File.join(Rails.root, 'config', 'envr.rb')
+load(app_test) if File.exist?(app_test)
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
